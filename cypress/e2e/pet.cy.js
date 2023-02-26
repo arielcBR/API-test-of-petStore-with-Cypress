068b0935312e947f6a8619c2d480b2c6963352dd
Ariel <ariel.campos94@yahoo.com>
2023-02-26 19:12:19 -0300

    loading pets by JSON

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -1,64 +1,56 @@
 describe('PET', () => {
-    let pet = {
-        'id': 7894,
-        'category': {
-            'id': 1,
-            'name': 'Cats'
-        },
-        'name': 'Chulo',
-        'photoUrls': ['string'],
-        'tags': [
-            {
-                'id': 1,
-                'name': 'cute'
-            }
-        ],
-        'status': 'available'
-    };
-
     it('POST PET', () => {
-        cy.request({
-            method: 'POST',
-            url: '/pet',
-            body: pet
-        }).then((res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.category.name).to.equal('Cats');
+        cy.fixture('pet.json').then((pet) => {
+            cy.request({
+                method: 'POST',
+                url: '/pet',
+                body: pet
+            }).then((res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body.category.name).to.equal('Cats');
+            });
         });
     });
 
     it('GET PET', () => {
-        cy.request({
-            method: 'GET',
-            url: `/pet/${pet.id}`,
-        }).then((res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.name).to.equal('Chulo');
-            expect(res.body.status).to.equal('available');
-            expect(res.body.id).to.equal(7894);
+        cy.fixture('pet.json').then((pet) => { 
+            cy.request({
+                method: 'GET',
+                url: `/pet/${pet.id}`,
+            }).then((res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body.name).to.equal('Chulo');
+                expect(res.body.status).to.equal('available');
+                expect(res.body.id).to.equal(7894);
+            });
         });
     });
 
     it('UPDATE PET', () => {
-        pet.status = 'unavailable';
 
-        cy.request({
-            method: 'PUT',
-            url: '/pet',
-            body: pet
-        }).then((res) => {
-            cy.log(res);
-            expect(res.status).to.equal(200);
-            expect(res.body.status).to.equal('unavailable');
+        cy.fixture('petUpdate.json').then((petUpdated) => {
+            cy.log(petUpdated);
+            cy.request({
+                method: 'PUT',
+                url: '/pet',
+                body: petUpdated
+            }).then((res) => {
+                cy.log(res);
+                expect(res.status).to.equal(200);
+                expect(res.body.status).to.equal('unavailable');
+            });
         });
+
     });
 
     it('DELETE PET', () => {
-        cy.request({
-            method: 'DELETE',
-            url: `/pet/${pet.id}`
-        }).then((res) => {
-            expect(res.body.code).to.equal(200);
+        cy.fixture('pet.json').then((pet) => { 
+            cy.request({
+                method: 'DELETE',
+                url: `/pet/${pet.id}`
+            }).then((res) => {
+                expect(res.body.code).to.equal(200);
+            });
         });
     });
 });
